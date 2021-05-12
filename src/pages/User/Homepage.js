7257117f081b9405c9ac420d40549edd59a98f92
Tran Thanh Tuan <tranttuan96@gmail.com>
2021-05-12 18:30:17 +0700

    init_branch_tuan

--- src/pages/User/Homepage.js
+++ src/pages/User/Homepage.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Homepage() {
     return (
         <div>
-            Trang chủ đặt món
+            Trang chủ đặt món - Test commit Branch
         </div>
     )
 }
