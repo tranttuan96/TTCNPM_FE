2bd9f03cda2161967c4e1b4318b5bab25d2f066e
Tran Phuong Tinh <tinh.p.tran@gmail.com>
2021-05-16 23:03:36 +0700

    add menu management function for manager

---
+++ src/pages/Admin/HomeAdmin.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import Table from "react-bootstrap/Table";
+// import Button from "react-bootstrap/Button";
+// import moment from "moment";
+// npm install react-bootstrap bootstrap
+// npm install moment
+
+export default class MyComponent extends React.Component {
+	render() {
+		return (
+			<h1>Welcome to admin page!</h1>
+		);
+	}
+}
