e0f9c015543a66e36487d276f64ed1397d4d759e
jamiemai <maicuong1701@gmail.com>
2021-05-21 01:52:08 +0700

    layout update stt food page

--- src/pages/Chef/Chefpage.js
+++ src/pages/Chef/Chefpage.js
@@ -2,12 +2,20 @@ import React, {useState, useEffect} from 'react'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 import "./Chefstyle.scss"
+import Modal from 'react-bootstrap/Modal'
 
 export default function Chefpage() {
 
    function say() {
        alert('Check!');
      }
+     
+     const [show, setShow] = useState(false);
+
+     const handleClose = () => setShow(false);
+     const handleShow = () => setShow(true);
+   
+    // const [modalShow, setModalShow] = React.useState(false);
 
    return (
        <section className ="container">
@@ -16,9 +24,9 @@ export default function Chefpage() {
                    <div className="one_order">
                    <Card>
                    {/* <Card.Header class="card-header d-flex justify-content-between ">OrderID</Card.Header> */}
-                   <Card.Body className="card-body d-flex justify-content-between" onClick={say} >
+                   <Card.Body className=" d-flex justify-content-between" >
                        <Card.Title >info 1</Card.Title>
-                       <Button type="button" variant="success" onClick={say} size="sm" >
+                       <Button type="button" variant="success"  onClick= {handleShow} size="sm" >
                        Confirm
                        </Button>
                    </Card.Body>
@@ -28,9 +36,9 @@ export default function Chefpage() {
                    <div className="one_order">
                    <Card>
                    {/* <Card.Header class="card-header d-flex justify-content-between ">OrderID</Card.Header> */}
-                   <Card.Body className="card-body d-flex justify-content-between " onClick={say} >
+                   <Card.Body className=" d-flex justify-content-between " >
                        <Card.Title >info 2</Card.Title>
-                       <Button type="button"className="btn btn-success" onClick={say} size="sm" >
+                       <Button type="button"className="btn btn-success" onClick= {handleShow} size="sm" >
                        Confirm
                        </Button>
                    </Card.Body>
@@ -42,7 +50,7 @@ export default function Chefpage() {
                    {/* <Card.Header class="card-header d-flex justify-content-between ">OrderID</Card.Header> */}
                    <Card.Body className="card-body d-flex justify-content-between " onClick={say} >
                        <Card.Title >info 3</Card.Title>
-                       <Button type="button"className="btn btn-success" onClick={say} size="sm" >
+                       <Button type="button"className="btn btn-success"  onClick={say} size="sm" >
                        Confirm
                        </Button>
                    </Card.Body>
@@ -55,41 +63,39 @@ export default function Chefpage() {
                <div className="order_view_panel_right col-md-12" >
                    <div className ="order_view horizontal-center ">This will show order</div>
                    <div className ="btn_div">
-                   <Button type="button"className="btn btn-secondary" size="sm"  onClick={say}>
+                   <Button type="button"className="btn btn-secondary" size="lg"  onClick={say}>
                            Cancel
                        </Button>{' '}
-                       <Button type="button" className="btn btn-success" size="sm" onClick={say} >
+                       <Button type="button" className="btn btn-success" size="lg" onClick={say} >
                            Print
                    </Button>{' '}
-                       <Button type="button"className="btn btn-success " size="sm"  onClick={say} >
+                       <Button type="button"className="btn btn-success " size="lg"   onClick={say} >
                            Confirm
                    </Button>{' '}
                    </div>
                </div>
            {/* </div>  */}
-       </section>
+           <Modal        
+                show={show}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+                centered
+                size="sm"
+                >
+                <Modal.Body>
+                    Are you sure?
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                    Close
+                    </Button>
+                    <Button variant="success">Confirm</Button>
+                </Modal.Footer>
+                </Modal>
+       </section> 
+
    )
 }
 
 
-
-// export default function ChefPage() {
-
-//     const [isConfirmed, setConfirmed] = useState(null);
-
-//     useEffect(() => {
-//       function handleStatusChange(status) {
-//         setConfirmed(status.isConfirmed);
-//       }
-//     //   ChatAPI.subscribeToOderStatus(props.order.id, handleStatusChange);
-//       // Specify how to clean up after this effect:
-//       return function cleanup() {
-//         // ChatAPI.unsubscribeFromFriendStatus(props.friend.id, handleStatusChange);
-//       };
-//     });
- 
-//     if (isConfirmed === null) {
-//       return 'Loading...';
-//     }
-//     return isConfirmed ? 'Complete' : 'Waiting';
-// }
