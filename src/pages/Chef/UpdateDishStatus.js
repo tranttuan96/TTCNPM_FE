2fe50cd4e1a916da4965bf5680a05c8bc32a8e1e
jamiemai <maicuong1701@gmail.com>
2021-06-13 20:47:41 +0700

    Change layout of UpdatePage, Fetching data, add actions

--- src/pages/Chef/UpdateDishStatus.js
+++ src/pages/Chef/UpdateDishStatus.js
@@ -106,33 +106,6 @@ export default class MyComponent extends React.Component {
     return data;
   }
 
-  async postData(url = "", formData) {
-    // Default options are marked with *
-    const response = await fetch(url, {
-      method: "POST", // *GET, POST, PUT, DELETE, etc.
-      mode: "cors", // no-cors, *cors, same-origin
-      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: "same-origin", // include, *same-origin, omit
-      // headers: {
-      // "Content-Type": "application/json",
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-      // },
-      redirect: "follow", // manual, *follow, error
-      referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      body: formData, // body data type must match "Content-Type" header
-    });
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    const response2 = await response.json(); // parses JSON response into native JavaScript objects
-
-    alert("Thêm món ăn thành công!");
-
-    return response2;
-  }
-
   async putData(url = "", formData) {
     // Default options are marked with *
     const response = await fetch(url, {
